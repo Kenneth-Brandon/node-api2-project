171cc1daa4d9307224f71f455ff73df2c3c98dc7
Patricia Fullmer <patriciafullmer@Patricias-MacBook-Pro.local>
2020-05-12 10:31:18 -0700

    router PUT

--- router/blogRouter.js
+++ router/blogRouter.js
@@ -71,4 +71,31 @@ router.delete('/:id', (request, response) => {
     });
 });
 
+router.put('/:id', (request, response) => {
+  const changes = request.body;
+  const { title, contents } = request.body;
+
+  if (!title || !contents) {
+    response.status(400).json({
+      errorMessage: 'Please provide title and contents for the post.',
+    });
+  }
+
+  db.update(request.params.id, changes)
+    .then((post) => {
+      if (post) {
+        response.status(200).json(post);
+      } else {
+        response.status(404).json({
+          message: 'The post with the specified ID does not exist.',
+        });
+      }
+    })
+    .catch((error) => {
+      response.status(500).json({
+        error: 'The post information could not be modified.',
+      });
+    });
+});
+
 module.exports = router;
