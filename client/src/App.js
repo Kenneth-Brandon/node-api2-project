5c402e2cf297c6c2fb552504e3743df6ee409108
Kenneth-Brandon <onegiantsfan25@yahoo.com>
2020-05-12 12:24:31 -0700

    created Post.js component and imported to app.js and installed axios

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Posts from './components/Posts';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Test</h1>
+      <Posts />
     </div>
   );
 }
